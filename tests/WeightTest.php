93cd79be4280a9d63e61bc8a1ddaf726cddd262a
freek <freek@spatie.be>
2021-11-03 13:43:46 +0100

    fix tests

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -5,5 +5,5 @@ use Spatie\WeightConversions\Weight;
 it('can convert kilograms to pounds correctly', function () {
     $pounds = Weight::kilograms(100)->toPounds();
 
-    expect($pounds)->toEqual(220.45);
+    expect($pounds)->toEqual(220.46);
 });
