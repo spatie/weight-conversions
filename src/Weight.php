7be7fe0cf25294b978c04c992a3b13760efe8156
freek <freek@spatie.be>
2021-11-03 15:23:28 +0100

    add toStones

--- src/Weight.php
+++ src/Weight.php
@@ -20,4 +20,9 @@ class Weight
     {
         return $this->kilograms * 2.2046;
     }
+
+    public function toStones(): float
+    {
+        return $this->kilograms * 0.15747;
+    }
 }
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -7,3 +7,9 @@ it('can convert kilograms to pounds correctly', function () {
 
     expect($pounds)->toEqual(220.46);
 });
+
+it('can convert kilograms to stones correctly', function () {
+    $pounds = Weight::kilograms(100)->toStones();
+
+    expect($pounds)->toEqual(15.747);
+});
